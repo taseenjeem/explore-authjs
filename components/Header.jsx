66a1bb59d838453cfe7d1a58bf127a74ccaf673d
taseenjeem <taseenjeem.dev@gmail.com>
2024-05-10 00:41:02 +0600

    added login info

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,28 @@
 import { auth } from "@/auth";
+import Image from "next/image";
 
 const Header = async () => {
   const session = await auth();
   console.log(session);
 
-  return <div>Hello World</div>;
+  return (
+    <>
+      {session?.user ? (
+        <div className="flex">
+          <p>{session?.user?.name}</p> |
+          <Image
+            src={session?.user?.image}
+            alt={session?.user?.name}
+            width={32}
+            height={32}
+            className="rounded-full"
+          />
+        </div>
+      ) : (
+        <p>Ask for login</p>
+      )}
+    </>
+  );
 };
 
 export default Header;
